7456de6fc18f18bc064d0eadb2ba0cd195b8a0c6
Denis Labaste <denis.labaste@gmail.com>
2014-10-14 23:46:29 +0200

    Css changed

--- js/config.js
+++ js/config.js
@@ -11,9 +11,9 @@ cfg = {
 	position: 'r1bqkbnr/pppp1ppp/2n5/1B2p3/4P3/5N2/PPPP1PPP/RNBQK2R w KQkq - 0 1',
 	ai: true,
 	highlight: true,
-	selectionable: true,
+	selectionable: false,
 	theme: {
-		w: 'default',
+		w: 'classic',
 		b: 'classic'
 	},
 	pieceTheme: pieceTheme
