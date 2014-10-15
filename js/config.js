c0785fd2296f90aeafed75a9c48ee1b9a6ecd89c
Denis Labaste <denis.labaste@gmail.com>
2014-10-15 17:02:35 +0200

    edit roque bug

--- js/config.js
+++ js/config.js
@@ -194,7 +194,7 @@ function init(cfg) {
 		boardEl.find('.square-' + source).addClass('highlight-white');
 		boardEl.find('.square-' + target).addClass('highlight-white');
 
-		if( ( game.fen().match(/.*r.*r.*R.*R.*KQ.*/g) ) == null ){
+		if( ( game.fen().match(/.*r.*r.*R.*R.*KQ.*/g) ) == null && (kk != '-') ){
 			document.getElementById('roque').classList.add('discover');
 		}
 
